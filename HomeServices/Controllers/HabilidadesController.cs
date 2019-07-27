e9aeac65fa010fe6afab15260de7a1e89cb99b8c
sebastian <sebastianduque1323@hotmail.com>
2019-07-27 13:32:15 -0500

    Comments

--- HomeServices/Controllers/HabilidadesController.cs
+++ HomeServices/Controllers/HabilidadesController.cs
@@ -9,11 +9,18 @@ using System.Web.Http;
 
 namespace HomeServices.Controllers
 {
+
+    /// <summary>Permite obtener y envíar los web services relacionados con el 
+    /// recurso Habilidades (generales), haciendo uso de arquitectura REST
+    /// </summary>
     public class HabilidadesController : ApiController
     {
 
         ServiciosHabilidad habilidades = new ServiciosHabilidad();
 
+
+        /// <summary>Permite 
+        /// </summary>
         [HttpGet]
         public IEnumerable<Object> Get(HttpRequestMessage request)
         {
